7e3f18e193574f037aff9b87337734475a6b7741
Alexander Kovalchuk <skateray17@gmail.com>
2018-04-12 16:03:35 +0300

    moved source files to src, added babel

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const accountRoter = require('./routes/account-routes');
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import accountRoter from './routes/account-routes';
 
 const app = express();
 
