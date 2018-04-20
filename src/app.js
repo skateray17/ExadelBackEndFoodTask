95d6144ea03152045184d29a2bc1d54920078ac0
Alexander Kovalchuk <skateray17@gmail.com>
2018-04-20 18:43:41 +0300

    added basic swagger

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import path from 'path';
 import accountRouter from './routes/account-routes';
 import menuRouter from './routes/menu-router';
 import orderRouter from './routes/user-orders-router';
@@ -16,6 +17,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/account', accountRouter);
 app.use('/api/menu', menuRouter);
 app.use('/api/order', orderRouter);
+
+app.get('/swagger/:params*', (req, res) => res.sendFile(path.resolve(`${__dirname}/../${req.path}`)));
 
 const server = app.listen(process.env.CONNECTION_PORT, () => {
   console.log(`Server listening on port ${server.address().port}`);
