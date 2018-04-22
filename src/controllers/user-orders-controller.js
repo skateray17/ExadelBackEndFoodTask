1113aef370241737a9637a3245e6d0200d45f262
Maxim Bykov <maxim12098@gmail.com>
2018-04-22 15:44:52 +0300

    order validation added

--- src/controllers/user-orders-controller.js
+++ src/controllers/user-orders-controller.js
@@ -1,5 +1,5 @@
 import UserOrders from '../models/UserOrders';
-
+import MenuLogic from '../controllers/menu-controller';
 
 function searchSuitableDays(username, days) {
   const buffer = [];
@@ -22,14 +22,41 @@ function getOrders(username) {
     result: searchSuitableDays(username, days),
   }));
 }
-
-function addOrder(obj) {
-  return UserOrders.findOneAndUpdate(
-    { date: obj.date },
-    { $push: { orders: obj.order } },
-    { new: true },
-  );
+function validateOrder(order) {
+  const MENU = MenuLogic.getMenu();
+  let sum = 0;
+  return Object.values(MENU.menu).some((item) => {
+    if (item.day === order.date) {
+      const availableItemsForOrder = item.menu.concat(MENU.menu.common.menu);
+      return order.order.dishList.every(dish => availableItemsForOrder.some((itemForOrder) => {
+        if (itemForOrder.name === dish.dishTitle) {
+          sum += itemForOrder.cost * dish.amount;
+          return true;
+        }
+        return false;
+      }));
+    }
+    return false;
+  }) && sum === order.order.totalPrice;
 }
 
+function addOrder(obj) {
+  if (validateOrder(obj)) {
+    return UserOrders.findOneAndUpdate(
+      { date: obj.date },
+      {
+        $set: {
+          orders: {
+            username: obj.order.username,
+            dishList: obj.order.dishList,
+            totalPrice: obj.order.totalPrice,
+          },
+        },
+      },
+      { new: true, upsert: true },
+    );
+  }
+  return new Error('Invalid order or there is no suitable menu');
+}
 export default { getOrders, addOrder };
 
