3896d1b74acb5dee7c9aa19fd88b99190e55163d
Alexander Kovalchuk <skateray17@gmail.com>
2018-04-23 19:22:56 +0300

    added authorization

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import bodyParser from 'body-parser';
 import accountRouter from './routes/account-routes';
 import menuRouter from './routes/menu-router';
 import orderRouter from './routes/user-orders-router';
+import authorization from './controllers/authorization';
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/account', accountRouter);
+app.use('/api/', authorization.authorizeUser);
 app.use('/api/menu', menuRouter);
 app.use('/api/order', orderRouter);
 
---
+++ src/controllers/authorization.js
@@ -0,0 +1,31 @@
+import accountController from './account-controller';
+import userTypes from '../models/user-types';
+
+export default {
+  authorizeUser, // eslint-disable-line
+  authorizeAdmin, // eslint-disable-line
+};
+
+function authorize(req, res, next, type) {
+  console.log(1);
+  (req.parsedToken ? new Promise(resolve => resolve(req.parsedToken))
+    : accountController.untokenize(req.headers.authorization))
+    .then((token) => {
+      req.parsedToken = token;
+      if (!token || token.type < type) {
+        return Promise.reject();
+      }
+      return next();
+    })
+    .catch(() => {
+      res.status(403).send('No permissions');
+    });
+}
+
+function authorizeUser(req, res, next) {
+  authorize(req, res, next, userTypes.BASIC_USER);
+}
+
+function authorizeAdmin(req, res, next) {
+  authorize(req, res, next, userTypes.ADMIN);
+}
