dcb233199182fa85b64cbc7d4051b0981148ede0
Maxim Bykov <maxim12098@gmail.com>
2018-05-02 16:46:26 +0300

    routers fix

--- src/routes/user-orders-router.js
+++ src/routes/user-orders-router.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import ordersController from '../controllers/user-orders-controller';
+import User from '../models/User';
 
 const router = express.Router();
 
 router.route('/')
   .get((req, res) => {
-    ordersController.getOrders(req.query.username, req.query.startDate, req.query.endDate)
+    User.findById({ _id: req.parsedToken.id })
+      .then(user => ordersController.getOrders(user.email, req.body.startDate, req.body.endDate))
       .then((response) => {
         res.status(200).send(response);
       })
@@ -14,9 +16,11 @@ router.route('/')
 
 router.route('/')
   .put((req, res) => {
-    ordersController.addOrder(req.body).then((response) => {
-      res.status(200).send(response);
-    })
+    User.findById({ _id: req.parsedToken.id })
+      .then((user) => { req.body.username = user; ordersController.addOrder(req.body); })
+      .then((response) => {
+        res.status(200).send(response);
+      })
       .catch((err) => { res.status(500).send(err); });
   });
 module.exports = router;
