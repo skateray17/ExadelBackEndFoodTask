0bb0c923a97812ce27501c2cb558d4e45a4ec7b6
Alexander Kovalchuk <skateray17@gmail.com>
2018-05-08 17:30:43 +0300

    fixed cors

--- src/app.js
+++ src/app.js
@@ -18,8 +18,10 @@ mongoose.connect(process.env.CONNECTION_STRING);
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, PUT, POST, DELETE, PATCH');
   next();
 });
+app.options('*', (req, res) => res.end());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
