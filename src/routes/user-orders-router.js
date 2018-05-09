12d2520dbc222d93d8fa0e13ce452072d2b94bf8
Maxim Bykov <maxim12098@gmail.com>
2018-05-09 12:18:23 +0300

    Added get orders by date opportunity

--- src/routes/user-orders-router.js
+++ src/routes/user-orders-router.js
@@ -1,17 +1,35 @@
 import express from 'express';
 import ordersController from '../controllers/user-orders-controller';
 import User from '../models/User';
+import authorizationController from '../controllers/authorization';
 
 const router = express.Router();
 
 router.route('/')
   .get((req, res) => {
-    User.findById({ _id: req.parsedToken.id })  
-      .then(user => ordersController.getOrders(user.email, req.body.startDate, req.body.endDate))
-      .then((response) => {
+    if (req.query.currentDate === undefined) {
+      User.findById({ _id: req.parsedToken.id })
+        .then((user) => {
+          let startDate;
+          let endDate;
+          if (typeof req.query.startDate === 'string') {
+            startDate = new Date(req.query.startDate);
+          }
+          if (typeof req.query.endDate === 'string') {
+            endDate = new Date(req.query.endDate);
+          }
+
+          ordersController.getOrders(user.email, startDate, endDate);
+        })
+        .then((response) => {
+          res.status(200).send(response);
+        })
+        .catch((err) => { res.status(500).send(err); });
+    } else {
+      ordersController.getOrdersByDate(new Date(req.query.currentDate)).then((response) => {
         res.status(200).send(response);
-      })
-      .catch((err) => { res.status(500).send(err); });
+      }).catch((err) => { res.status(500).send(err); });
+    }
   });
 
 router.route('/')
