fdf0fa7387e84af09b16060c74df21f2780b2288
Maxim Bykov <maxim12098@gmail.com>
2018-05-21 23:20:36 +0300

    timezone bugs fixed

--- src/controllers/user-orders-controller.js
+++ src/controllers/user-orders-controller.js
@@ -1,31 +1,61 @@
 import UserOrders from '../models/UserOrders';
 import MenuController from '../controllers/menu-controller';
 
-
-function addDaysToMidnight(date, daysToAdd) {
-  return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getUTCDate() + daysToAdd));
-}
-
-function getMidnight(date) {
-  return new Date(date.getFullYear(), date.getMonth(), date.getUTCDate());
+// ПРИСЫЛАТЬ ВСЕ БЕЗ Z В КОНЦЕ!!!!!
+function setMidnight(date) {
+  date.setHours(0);
+  date.setMinutes(0, 0, 0);
 }
 
 function addDaysToDate(date, daysToAdd) {
-  return date.setUTCDate(date.getUTCDate() + daysToAdd);
+  date.setDate(date.getDate() + daysToAdd);
 }
 
-function getOrders(
-  username, startDate = addDaysToMidnight(new Date(), 0),
-  endDate = addDaysToMidnight(new Date(), 7),
-) {
-  return UserOrders.find({ username, date: { $gte: startDate, $lte: endDate } })
+function castToValidTimezone(date) {
+  date.setHours(date.getHours() + 3);
+}
+
+function getOrders(username, dates) {
+  let startDate;
+  let endDate;
+  if (dates.startDate && dates.endDate) {
+    startDate = new Date(dates.startDate);
+    castToValidTimezone(startDate);
+    setMidnight(startDate);
+
+    endDate = new Date(dates.endDate);
+    castToValidTimezone(endDate);
+    setMidnight(endDate);
+    return UserOrders.find({ username, date: { $gte: startDate, $lte: endDate } })
+      .then(obj => ({ result: obj }));
+  } else if (dates.startDate) {
+    startDate = new Date(dates.startDate);
+    castToValidTimezone(startDate);
+    setMidnight(startDate);
+
+    return UserOrders.find({ username, date: { $gte: startDate } })
+      .then(obj => ({ result: obj }));
+  } else if (dates.endDate) {
+    endDate = new Date(dates.endDate);
+    castToValidTimezone(endDate);
+    setMidnight(endDate);
+
+    return UserOrders.find({ username, date: { $lte: endDate } })
+      .then(obj => ({ result: obj }));
+  }
+  return UserOrders.find({ username })
     .then(obj => ({ result: obj }));
 }
 
 
 function isOrderValid(order, currentDate) {
-  const maxTime = addDaysToMidnight(new Date(), 14);
-  const minTime = addDaysToMidnight(new Date(), 0);
+  const maxTime = new Date();
+  castToValidTimezone(maxTime);
+  addDaysToDate(maxTime, 14);
+  setMidnight(maxTime);
+  const minTime = new Date();
+  castToValidTimezone(minTime);
+  setMidnight(minTime);
   let sum = 0;
   const MENU = MenuController.getCommonByDate(currentDate)
     .concat(MenuController.getMenuByDate(currentDate));
@@ -46,7 +76,9 @@ function isOrderValid(order, currentDate) {
 }
 
 function validateOrder(order) {
-  const currentDate = addDaysToMidnight(new Date(order.date), 0);
+  const currentDate = new Date(order.date);
+  castToValidTimezone(currentDate);
+  setMidnight(currentDate);
   const sum = isOrderValid(order, currentDate);
   if (sum) {
     return Promise.resolve({
@@ -68,12 +100,14 @@ function addOrder(order) {
       },
     },
     { new: true, upsert: true },
-  ).then(() => (Promise.resolve({ totalPrice: obj.totalPrice })))
-    .catch(() => Promise.reject(new Error())));
+  ).then(() => (Promise.resolve({ totalPrice: obj.totalPrice }))));
 }
 
 function getOrdersByDate(date) {
-  const currentDate = addDaysToMidnight(date, 0);
+  const currentDate = new Date(date);
+  castToValidTimezone(currentDate);
+  setMidnight(currentDate);
+
   return UserOrders.find({ date: { $eq: currentDate } })
     .then(obj => ({ result: obj }));
 }
