1b5a2eba96378d71df42174aabee3263b0a13fc6
Egor Karabanovich <karabanovich333@gmail.com>
2018-05-23 15:43:49 +0300

    add GetUserBalance

--- src/routes/balance-router.js
+++ src/routes/balance-router.js
@@ -7,19 +7,29 @@ const router = express.Router();
 router.route('/')
   .get(authorization.authorizeAdmin)
   .get((req, res) => {
-    balanceController.findUsers(req.query.name, Number.parseInt(req.query.page, 10))
-      .then((response) => {
-        res.status(200).send(response);
-      })
-      .catch((err) => {
-        res.status(500).send(err);
-      });
+    if ('username' in req.query) {
+      balanceController.findUserByUsername(req.query.username)
+        .then((response) => {
+          res.status(200).send(response);
+        })
+        .catch((err) => {
+          res.status(500).send(err);
+        });
+    } else {
+      balanceController.findUsers(req.query.name, Number.parseInt(req.query.page, 10))
+        .then((response) => {
+          res.status(200).send(response);
+        })
+        .catch((err) => {
+          res.status(500).send(err);
+        });
+    }
   })
   .put(authorization.authorizeAdmin)
   .put((req, res) => {
     balanceController.updateUserBalance(req.body.username, req.body.balance)
-      .then(() => {
-        res.status(200).send();
+      .then((response) => {
+        res.status(200).send(response);
       })
       .catch((err) => {
         res.status(500).send(err);
