b5848a192850ac01ce9331fe4f16cea488698326
Pavel Ramasheuski <pavel.ramasheuski@gmail.com>
2018-08-02 13:31:24 +0300

    added menu log

---
+++ src/models/Messages.js
@@ -0,0 +1,9 @@
+export default {
+  makeOrder: 'Order have been made',
+  removeOrder: 'Order have been removed',
+  updateOrder: 'Order have been updated',
+  uploadMenu: 'Menu have been uploaded',
+  publishMenu: 'Menu have been published',
+  disableDay: 'Day have been disabled',
+  removeMenu: 'Menu have been removed',
+};
