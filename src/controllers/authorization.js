b20634aed30bb679e185fdf7adb539443327a659
L-u-k-o-s <31861712+L-u-k-o-s@users.noreply.github.com>
2018-08-23 15:28:40 +0300

    code style improvements

--- src/controllers/authorization.js
+++ src/controllers/authorization.js
@@ -1,19 +1,14 @@
-import axios from 'axios/index';
-import ExternalLinks from '../models/extrnal-links';
+import accountController from '../controllers/account-controller';
 
 export default {
   authorizeAdmin, // eslint-disable-line
 };
 
 function authorizeAdmin(req, res, next) {
-  return axios(`${ExternalLinks.rvisionLink}/security/getLoginStatus`, {
-    headers: { cookie: req.headers.cookie },
-    method: 'GET',
-  }).then((response) => {
+  return accountController.getLoginStatus(req).then((response) => {
     if (response.data.permissions.includes('efds_admin')) {
       return next();
     }
     return res.status(401).end('NO PERMISSION');
   }).catch(() => res.status(500).end());
-  // authorize(req, res, next, userTypes.ADMIN);
 }
